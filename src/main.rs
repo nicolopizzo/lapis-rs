223b30634b370ebc1fba164ff45364ba7001238a
Nicolò Pizzo <nicolopizzo@outlook.com>
2023-12-11 22:51:17 +0100

    added main access point

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 use core::panic;
 use std::{collections::HashMap, env, process::exit, rc::Rc};
 
-use parser::{Context, Rewrite};
+use check::check_context;
+use parser::{Context, Rewrite, parse};
 
 mod check;
 mod lgraph;
@@ -11,28 +12,36 @@ mod lnode;
 mod parser;
 mod utils;
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() <= 1 {
-    // println!("ERROR: YOU MUST SPECIFY A TARGET FILE");
-    // println!("Example usage: lapis <FILEPATH>");
-    // }
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        println!("ERROR: YOU MUST SPECIFY A TARGET FILE");
+        println!("Example usage: lapis <FILEPATH>");
+    }
 
-    // let filepath = &args[1];
-    // let idx = filepath.rfind("/");
-    // if let Some(idx) = idx {
-    // let path = &filepath[0..idx];
-    // if let Err(e) = env::set_current_dir(path) {
-    // panic!("Error: {:?}", e);
-    // }
-    // }
+    let filepath = &args[1];
+    let idx = filepath.rfind("/");
+    if let Some(idx) = idx {
+        let path = &filepath[0..idx];
+        if let Err(e) = env::set_current_dir(path) {
+            panic!("Error: {:?}", e);
+        }
+    }
 
-    // let idx = idx.unwrap_or(0);
-    // let filename = &filepath[idx + 1..];
+    let idx = idx.unwrap_or(0);
+    let filename = &filepath[idx + 1..];
 
-    // let ctx = parse(filename);
-    // let check = check_context(ctx);
+    let ctx = parse(filename);
+    let check = check_context(&ctx);
 
-    // if let Err(e) = check {
-    // println!("Error occurred: {:?}", e);
-    // }
+    match check {
+        Ok(_) => {
+            println!("Successful check");
+        }
+
+        Err(e) => {
+            println!("Check failed");
+            println!("Error: {:?}", e);
+
+        }
+    }
 }
